39b3c26aae58d735ebbf81342e92300cf3f8fbea
IAmVane <vane.charles24@gmail.com>
2017-08-19 10:28:43 -0400

    more styling added font and icons

--- paintnite-events-app/src/EventCard.js
+++ paintnite-events-app/src/EventCard.js
@@ -1,23 +1,51 @@
 import React from 'react';
 
+
 const EventCard =(props)=> {
 
     return (
       <div className="EventCard col-md-4">
-       <div className="event-wrapper">
-    
-        date{props.date}
-        
-        <img src={props.image}/>
-        distance{props.distance}
-        tickets{props.tickets}
-        name{props.name}<br/>
-        venue{props.venue}<br/>
-        <img className="avatar" src={props.artistImage}/>
        
-    {props.firstName}
-    {props.lastName}
-      </div>
+       <div className="event-wrapper">
+        
+        <div className="date">
+         
+         date{props.date}
+        
+        </div>
+        
+        <img alt="event" src={props.image} className="event-image"/>
+        
+        <div className="col-md-6">
+         
+          <img alt="distance" className="icon" src={require('./img/map.png')}/>
+        
+          {Number(Math.round(props.distance+'e1')+'e-1')}
+        
+        </div>
+        
+        <div className="col-md-6">
+          
+          <img alt="ticket" className="icon" src={require('./img/ticket.png')}/>
+          
+          {props.tickets}
+        
+        </div>
+
+        <div className="event-details">
+          
+           <h3>{props.name}</h3>
+          
+          {props.venue}<br/>
+          <img alt="artist" className="avatar" src={props.artistImage}/>
+         
+          <span>{props.firstName}  {props.lastName}</span>
+         
+        
+        </div>
+      
+       </div>
+      
       </div>
     );
 }
