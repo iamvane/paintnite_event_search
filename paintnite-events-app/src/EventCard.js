7f48de303238f88bfc0c69d3dfc1e1525cbf8ae4
IAmVane <vane.charles24@gmail.com>
2017-08-19 11:44:51 -0400

    mor styling

--- paintnite-events-app/src/EventCard.js
+++ paintnite-events-app/src/EventCard.js
@@ -1,47 +1,70 @@
 import React from 'react';
+import moment from 'moment';
+
+
 
 
 const EventCard =(props)=> {
+
+  
+  const dateMonth= moment(props.date).format('MMMM DD'); // August 19th 2017, 10:56:24 am
+  const dayTime= moment(props.date).format('ddd, h:mm a'); // August 19th 2017, 10:56:24 am
+
+  const headerStyle = {
+    backgroundImage: 'url(' + props.image + ')', 
+    height: '280px',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    borderRadius: '10px 10px 0px 0px',
+    position: 'relative',
+    backgroundPosition: 'center'
+  };
 
     return (
       <div className="EventCard col-md-4">
        
        <div className="event-wrapper">
         
-        <div className="date">
-         
-         date{props.date}
-        
-        </div>
-        
-        <img alt="event" src={props.image} className="event-image"/>
-        
-        <div className="col-md-6">
-         
-          <img alt="distance" className="icon" src={require('./img/map.png')}/>
-        
-          {Number(Math.round(props.distance+'e1')+'e-1')}
-        
-        </div>
-        
-        <div className="col-md-6">
+        <div className="event-header" style={headerStyle}>
           
-          <img alt="ticket" className="icon" src={require('./img/ticket.png')}/>
+          <div className="date">
+           
+           <span className="date-month">{dateMonth}</span>
+           <span className="color-grey">{dayTime}</span>
           
-          {props.tickets}
-        
+          </div>
+          
+          <div className="event-meta">
+          
+            <div className="col-md-6">
+             
+              <img alt="distance" className="icon" src={require('./img/map.png')}/>
+            
+              <span>{Number(Math.round(props.distance+'e1')+'e-1')} MI</span>
+
+            </div>
+            
+            <div className="col-md-6">
+              
+              <img alt="ticket" className="icon" src={require('./img/ticket.png')}/>
+              
+              <span>{props.tickets} TIX</span>
+            
+            </div>
+
+          </div>
+       
         </div>
 
         <div className="event-details">
           
-           <h3>{props.name}</h3>
+           <h4>{props.name}</h4>
           
-          {props.venue}<br/>
+          <p className="color-grey">{props.venue}</p>
           <img alt="artist" className="avatar" src={props.artistImage}/>
          
           <span>{props.firstName}  {props.lastName}</span>
          
-        
         </div>
       
        </div>
